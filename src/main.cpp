9ffa2a45db80a7560de2ac7908e594a8c6869288
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-08-03 23:20:32 -0400

    created main func

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include "./components/typedefs.h"
 #include "./components/transistors/PMOS.h"
+#include "./utils/output.h"
 
 int main(int argc, char const *argv[])
 {
 
     PMOS pmos;
-    bool *input = new bool[1] {true};
-    bool *gate = new bool[1] {true};
     
-    bool *inp[2] = {input, gate};
+    component_io_t inp {{"input", {true}}, {"gate", {true}}};
+    component_io_t out = pmos.process(inp);
 
-    bool **out = pmos.process(inp);
+    std::cout << "Input: " << std::endl;
+    printComponentIO(inp);
+
+    std::cout << "Output: " << std::endl;
+    printComponentIO(out);
 
     return 0;
 }
