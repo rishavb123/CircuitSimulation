b5671e5fe25e0bc49f3c70ffb4133c1c7ba87a90
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-08-03 22:40:49 -0400

    moved implementations to h file

--- src/utils/arrayutils.h
+++ src/utils/arrayutils.h
@@ -4,12 +4,26 @@
 #include <iostream>
 
 template <typename T>
-int length(T *arr);
+int length(T *arr) {
+    return sizeof(arr) / sizeof(T);
+}
 
 template <typename T>
-void printArray(T *arr);
+void printArray(T *arr) {
+    int len = length(arr);
+    for (int i = 0; i < len; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
 template <typename T>
-void print2DArray(T **arr);
+void print2DArray(T **arr) {
+    int len = length(arr);
+    for (int i = 0; i < len; i++) {
+        printArray(arr[i]);
+    }
+}
 
 #endif // !ARRAY_UTILS_H
