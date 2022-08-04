6162d109e23f5dd3d9d67f1baabc55b9642d7042
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-08-04 14:45:12 -0400

    removed splitter tests

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,7 @@ int main(int argc, char const *argv[])
     UNUSED(argc);
     UNUSED(argv);
 
-    Splitter splitter({
-        {{0, 0}, {0, 1}, {0, 2}},
-        {{0, 3}, {1, 0}, {1, 1}},
-        {{1, 2}, {1, 3}, {2, 0}},
-        {{2, 1}, {2, 2}, {2, 3}}
-    });
-
-    component_io_t inp{
-        {"input_0", {1, 0, 0, 1}},
-        {"input_1", {0, 0, 1, 1}},
-        {"input_2", {1, 1, 0, 1}}
-    };
-
-    component_io_t out = splitter.process(inp);
-
-    std::cout << "Input: " << std::endl;
-    printComponentIO(inp, "\t");
-    std::cout << "Output: " << std::endl;
-    printComponentIO(out, "\t");
+    
 
     return 0;
 }
