26c81a16c779fe1c378415f277c92f963787b2d5
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-08-04 15:34:44 -0400

    change input

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
     UNUSED(argv);
 
     Constant *c1 = new Constant({1, 0});
-    Constant *c2 = new Constant({0, 1});
+    Constant *c2 = new Constant({1, 0});
 
     component_io_t inp({{"input_0", c1->getOutput()["output"]}, {"input_1", c2->getOutput()["output"]}});
 
