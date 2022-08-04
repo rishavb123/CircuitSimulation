95fcc73bb128c8beb6b9dbb3aa6be39bf0a16095
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-08-04 15:26:04 -0400

    testing junction and change inp validation

--- src/components/wiring/Junction.cpp
+++ src/components/wiring/Junction.cpp
@@ -19,16 +19,28 @@ bool Junction::validateInput(component_io_t inp) const
         {
             outDim = inp["input_" + std::to_string(i)].size();
         }
-        else if (outDim != inp["input_" + std::to_string(i)].size())
+        else if (outDim != (int)inp["input_" + std::to_string(i)].size())
         {
             return false;
         }
     }
     for (int j = 0; j < outDim; j++)
     {
+        bit_t val = 0;
+        bool first = true;
         for (size_t i = 1; i < this->numInputs; i++)
         {
-            if (inp.contains("input_" + std::to_string(i)) && inp["input_" + std::to_string(i)][j] != inp["input_" + std::to_string(i - 1)][j])
+
+            if (!inp.contains("input_" + std::to_string(i)))
+            {
+                continue;
+            }
+            if (first)
+            {
+                first = false;
+                val = inp["input_" + std::to_string(i)][j];
+            }
+            if (inp["input_" + std::to_string(i)][j] != val)
             {
                 return false;
             }
@@ -48,4 +60,5 @@ component_io_t Junction::getOutput()
             return out;
         }
     }
+    return out;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "./components/typedefs.h"
-#include "./components/wiring/Splitter.h"
+#include "./components/wiring/Constant.h"
+#include "./components/wiring/Junction.h"
 #include "./utils/macros.h"
 #include "./utils/output.h"
 
@@ -9,7 +10,13 @@ int main(int argc, char const *argv[])
     UNUSED(argc);
     UNUSED(argv);
 
-    
+    Constant *c1 = new Constant({1, 0});
+    Constant *c2 = new Constant({0, 1});
+
+    Junction *j = new Junction(2);
+    component_io_t out = j->process({{"input_0", c1->getOutput()["output"]}, {"input_1", c2->getOutput()["output"]}});
+
+    printComponentIO(out);
 
     return 0;
 }
