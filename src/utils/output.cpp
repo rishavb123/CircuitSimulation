a6b0b262a9d3ce252c697f114d1650f2836225bb
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-08-03 23:28:58 -0400

    added const for value

--- src/utils/output.cpp
+++ src/utils/output.cpp
@@ -7,7 +7,7 @@ void printComponentIO(component_io_t inp)
     for (auto& [name, values] : inp)
     {
         std::cout << name << ": ";
-        for (auto& value : values)
+        for (const auto& value : values)
         {
             std::cout << value << " ";
         }
